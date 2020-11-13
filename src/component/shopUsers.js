de014ae63a29a4d2c148af667f7782de1cbae57b
Andrew <jayedeem@gmail.com>
2020-11-13 10:10:50 -0800

    filtering by tags === 'employee'

--- src/component/shopUsers.js
+++ src/component/shopUsers.js
@@ -2,10 +2,26 @@ import { InputRightElement } from '@chakra-ui/core';
 import React from 'react';
 
 const ShopUsers = ({ users }) => {
-  const filteredUsers = users.customers[0].filter(
+  const filteredUsers = users.customers.filter(
     (user) => user.tags === 'employee'
   );
-  return <div>{JSON.stringify(filteredUsers, null, 4)}</div>;
+
+  return (
+    <div>
+      {filteredUsers.map((user) => {
+        return (
+          <div key={user.id}>
+            <ul>
+              <li>
+                <a href={user.id}>{user.id} </a> {user.first_name}{' '}
+                {user.last_name}
+              </li>
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ShopUsers;
